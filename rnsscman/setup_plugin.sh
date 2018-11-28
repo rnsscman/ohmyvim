0d351cc8e7d318922d93c3dcb6c8431b029c7101
yjroh <yjroh@solid.co.kr>
2018-11-28 10:53:37 +0900

    minor changes

--- rnsscman/setup_plugin.sh
+++ rnsscman/setup_plugin.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-
 if [ -e ./plugin_script.vim/cscope_maps.vim ]; then
     if [ ! -e ~/.vim/plugin/cscope_maps.vim ]; then
         cp -rv ./plugin_script.vim/cscope_maps.vim ~/.vim/plugin/cscope_maps.vim
@@ -14,4 +10,12 @@ if [ -e ./plugin_script.vim/gtags.vim ]; then
     if [ ! -e ~/.vim/plugin/gtags.vim ]; then
         cp -rv ./plugin_script.vim/gtags.vim ~/.vim/plugin/gtags.vim
     fi
+fi
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+if [ -d ~/.vim/bundle/Vundle.vim ]; then
+    vim +PluginInstall +qall
 fi
