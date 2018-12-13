f9106f4ff18d533e19205b400e6ba6e33f1b4596
rnsscman <rnsscman@gmail.com>
2018-12-13 14:29:30 +0900

    setup bug fix

--- .setup_cscope.sh
+++ .setup_cscope.sh
@@ -5,5 +5,6 @@ if [ -e $PWD/plugin_script/cscope_maps.vim ]; then
     if [ -e ~/.vim/plugin/cscope_maps.vim ]; then
         mv -v ~/.vim/plugin/cscope_maps.vim ~/.vim/plugin/cscope_maps.vim.org
     fi
+    mkdir -pv ~/.vim/plugin
     cp -rv $PWD/plugin_script/cscope_maps.vim ~/.vim/plugin/cscope_maps.vim
 fi
--- .setup_gtags.sh
+++ .setup_gtags.sh
@@ -5,5 +5,6 @@ if [ -e $PWD/plugin_script/gtags.vim ]; then
     if [ -e ~/.vim/plugin/gtags.vim ]; then
         mv -v ~/.vim/plugin/gtags.vim ~/.vim/plugin/gtags.vim.org
     fi
+    mkdir -pv ~/.vim/plugin
     cp -rv $PWD/plugin_script/gtags.vim ~/.vim/plugin/gtags.vim
 fi
